6b644c3a1a29385b2c165a0b01c31a63f57ce8d2
yeschan <yeschan@foxmail.com>
2022-01-13 20:33:21 +0800

    day11 stack and malloc

--- C/day11/stack.c
+++ C/day11/stack.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
+#include <string.h>
 char *print_stack()
 {
     char c[] = "hello world";
+    return c;
+}
+char *print_malloc()
+{
+    char *c;
+    c = (char*) malloc(20);
+    strcpy(c,"hello world");
     return c;
 }
 int main()
@@ -10,8 +18,10 @@ int main()
     
     char *p; p=c;
     printf("%p\t%s\n", p,p);
-    p=print_stack();
-    printf("%p\t%s\n", p,p);
-
+    // p=print_stack();//function will free the stack
+    // printf("%p\t%s\n", p,p);  
+    p=print_malloc();
+    printf("%p\t%s\n", p,p); //function do not free malloc
+    
     return 0;
 }
